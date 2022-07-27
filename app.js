4ed2b9ee0d83835f4f8c0ebbdaf8a2859ae547f4
Jonathan <jonathanmontiel055@gmail.com>
2022-07-27 17:40:05 -0500

    Finalizado

---
+++ app.js
@@ -0,0 +1,22 @@
+let luces = document.querySelectorAll(".circulo-semaforo");
+let cant_luces = 2;
+let contador = 0;
+let luz_actual;
+
+
+
+function anima(){
+    luces[contador].className="circulo-semaforo";
+    contador++;
+    contador = contador>cant_luces?0:contador;
+   
+  
+    luz_actual = luces[contador];
+    luz_actual.classList.add(luz_actual.getAttribute("color")) ;
+    
+
+
+
+
+}
+setInterval(anima,1000*5)
